a36fd264cda7b4094cb18634cfe2963c41f6e45a
xuchen <yaoxuchen@gmail.com>
2025-06-23 14:23:35 -0700

    handle SPA redirect

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,8 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 import Homepage from './components/Homepage'
 import TokenEpochApp from './TokenEpochApp'
 import TokenEpochAppZh from './TokenEpochAppZh'
+import { useEffect } from 'react';
 import './App.css'
+
+<RedirectHandler />
 
 function App() {
   return (
@@ -17,3 +21,18 @@ function App() {
 }
 
 export default App
+
+
+function RedirectHandler() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const redirect = params.get('redirect');
+    if (redirect) {
+      navigate(redirect, { replace: true });
+    }
+  }, [navigate]);
+  return null;
+}
+
+// In your App component, render <RedirectHandler /> at the top
